8531066e830192db59532e19d877453e3ff22277
Vincent1837 <86534155+Vincent1837@users.noreply.github.com>
2022-04-15 22:36:03 +0800

    MainGui Commit Test
    把按鈕從"Hello"改成"Hello World"
    Title  "Hello!" -> "Hello World!"

--- src/main/java/cpe_client/main/MainGui.java
+++ src/main/java/cpe_client/main/MainGui.java
@@ -12,7 +12,7 @@ public class MainGui extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainGui.class.getResource("Main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello World!");
         stage.setScene(scene);
         stage.show();
     }
