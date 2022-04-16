3910f61e8ead4763ca7b9304a72d775ff7a740f3
chenlitw <ab0897867564534231@gmail.com>
2022-04-16 09:55:29 +0800

    Add autosignin module - detect os and load correct chrome driver - add autoLoginandGetCookie

---
+++ src/main/java/cpe_client/autosignup/account.java
@@ -0,0 +1,36 @@
+package cpe_client.autosignup;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class account {
+
+    public static String popLoginPageandGetCookie(){
+        if(System.getProperty("os.name").equals("Mac OS X")){//better than Windows
+            System.setProperty("webdriver.chrome.driver","./webdriver/chromedriver");
+        }else{
+            System.setProperty("webdriver.chrome.driver","./webdriver/chromedriver.exe");
+        }
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://cpe.cse.nsysu.edu.tw/cpe/");
+
+        while (true){
+            if(driver.getCurrentUrl().equals("https://cpe.cse.nsysu.edu.tw/cpe/newest")){
+                break;
+            }
+            try {
+                Thread.sleep(100);
+            }catch (Exception e){
+
+            }
+        }
+
+        String ret=driver.manage().getCookies().toString();
+
+        driver.close();
+
+        return ret;
+
+    }
+}
