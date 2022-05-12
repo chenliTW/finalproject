ca807d57ece99596129a7d8cf6c284170c62c540
chenlitw <ab0897867564534231@gmail.com>
2022-05-12 22:10:36 +0800

    - src/main/java/cpe_client/autosignup/account.java     chromedriver加入custom     user-agent(不確定有沒有成功,cpe會驗證useragent 而且是在直接serialize的 php object XD)
    - src/main/java/cpe_client/autosignup/signup.java
         public static void signup()
            自動用引導使用者登入後拿到的cookie報名cpe (目前只有 東華大學 資工 報名 東華大學場)

--- src/main/java/cpe_client/autosignup/account.java
+++ src/main/java/cpe_client/autosignup/account.java
@@ -2,22 +2,26 @@ package cpe_client.autosignup;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 public class account {
 
-    public static String cookie="";
+    public static String cookie="";//ci_session
 
     public static String popLoginPageandGetCookie(){
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36");
+
         if(System.getProperty("os.name").equals("Mac OS X")){//better than Windows
             System.setProperty("webdriver.chrome.driver","./webdriver/chromedriver");
         }else{
             System.setProperty("webdriver.chrome.driver","./webdriver/chromedriver.exe");
         }
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://cpe.cse.nsysu.edu.tw/cpe/");
 
         while (true){
+
             if(driver.getCurrentUrl().equals("https://cpe.cse.nsysu.edu.tw/cpe/newest")){
                 break;
             }
@@ -32,9 +36,8 @@ public class account {
 
         driver.close();
 
-        account.cookie=ret;
-
-        return ret;
+        account.cookie=ret.split("; ")[0].split("ci_session=")[1];
+        return account.cookie;
 
     }
 }
---
+++ src/main/java/cpe_client/autosignup/signup.java
@@ -0,0 +1,65 @@
+package cpe_client.autosignup;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.security.SecureRandom;
+
+
+public class signup {
+
+    private static TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+    };
+
+    public static void signup() {
+
+        try {
+            CookieHandler.setDefault(new CookieManager());
+
+            HttpCookie sessionCookie = new HttpCookie("ci_session", account.cookie);
+
+            sessionCookie.setPath("/");
+            sessionCookie.setVersion(0);
+
+            ((CookieManager) CookieHandler.getDefault()).getCookieStore().add(new URI("https://cpe.cse.nsysu.edu.tw/"),
+                    sessionCookie);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+            HttpClient httpClient=HttpClient.newBuilder().cookieHandler(CookieHandler.getDefault()).sslContext(sslContext).build();//.cookieHandler(CookieHandler.getDefault()).build();
+
+            String data="isSend=yes&mySchoolID=45&myDepartment=%E8%B3%87%E8%A8%8A%E5%B7%A5%E7%A8%8B%E5%AD%B8%E7%B3%BB&myGrade=111&optionsRadios1=1&site=21&yesExam=%E5%A0%B1%E5%90%8D";
+
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://cpe.cse.nsysu.edu.tw/cpe/newest"))
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36")
+                    .POST(HttpRequest.BodyPublishers.ofString(data)).build();
+            while(true) {
+                try {
+                    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                    break;
+                }catch (Exception E){
+
+                }
+            }
+        }catch (Exception E){
+            System.out.println(E);
+        }
+    }
+
+}
