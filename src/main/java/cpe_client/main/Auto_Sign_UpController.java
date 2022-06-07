10c8ea40a4176920456daa89475e90b7983fa77a
Vincent1837 <86534155+Vincent1837@users.noreply.github.com>
2022-06-07 21:01:27 +0800

    自動報名 Controller update
    使用者能輸入學校、系級
    加入登入跟返回按鈕

--- src/main/java/cpe_client/main/Auto_Sign_UpController.java
+++ src/main/java/cpe_client/main/Auto_Sign_UpController.java
@@ -1,14 +1,27 @@
 package cpe_client.main;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 public class Auto_Sign_UpController {
     @FXML
-    private Label welcomeText;
+    private Button loginButton;
+    private Button backButton;
+    public ComboBox examLocationSelector;
+    public TextField schoolTextField;
+    public TextField departmentTextField;
+    public TextField gradeTextField;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onloginButtonClick() {
+        return;
+    }
+
+    @FXML
+    protected void onbackButtonClick() {
+        MainGui.currentStage.setScene(MainGui.menuScene);
     }
 }
